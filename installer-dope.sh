4844c9cc2641966f3931e1e0959dc0d6858fd66b
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2019-11-05 19:47:22 +0100

    added menu

--- installer-dope.sh
+++ installer-dope.sh
@@ -1,32 +1,66 @@
 #!/bin/bash
-unset version
-
-sudo apt update
-sudo apt upgrade
-
-
-version="$(lsb_release -sr)"
-case $version in
-	14.*)
-	version="libicu52"
-	;;
-	16.*)
-	version="licubu55"
-	;;
-	17.*)
-	version="licubu57"
-	;;
-	18.*)
-	version="libicu60"
-	;;
-	*)
-	echo "Unsupported ubuntu version"
-	exit 1
-	;;
-esac
-
-sudo apt install curl libssl1.0.0 libkrb5-3 zlib1g libunwind8 libuuid1 libc6 libstdc++6 libgcc1 $version
-cd
-mkdir DOPE
-cd DOPE
-wget -N "https://powerofdark.space/downloads/DOPE/0.42/DOPE.cli"
+PS3='Please enter your choice: '
+options=("Install Depandancies" "Download/Update DOPE" "Run DOPE" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Install Depandancies")
+            echo "you chose choice $REPLY which is $opt"
+            sleep 5
+            unset version
+            sudo apt update
+            sudo apt upgrade
+            version="$(lsb_release -sr)"
+            case $version in
+            	14.*)
+            	version="libicu52"
+            	;;
+            	16.*)
+            	version="licubu55"
+            	;;
+            	17.*)
+            	version="licubu57"
+            	;;
+            	18.*)
+            	version="libicu60"
+            	;;
+            	*)
+            	echo "Unsupported ubuntu version"
+            	exit 1
+            	;;
+            esac
+            sudo apt install curl libssl1.0.0 libkrb5-3 zlib1g libunwind8 libuuid1 libc6 libstdc++6 libgcc1 python-requests python python3-pip python-pip $version
+            sudo pip install wget
+            clear
+            echo "Depandancies installed succesfully"
+            sleep 1
+            echo "1) Install Depandancies 3) Run DOPE"
+            echo "2) Download/Update DOPE 4) Quit"
+            ;;
+        "Download/Update DOPE")
+            echo "you chose choice $REPLY which is $opt"
+            sleep 5
+            cd
+            mkdir -p DOPE
+            cd DOPE
+            wget -N "https://raw.githubusercontent.com/Gagong/Toshinou-Revamped/master/AutoUpdater.py" && python AutoUpdater.py
+            chmod +x ./DOPE/DOPE.cli
+            clear
+            echo "Downloaded/Updated succesfully"
+            sleep 3
+            echo "1) Install Depandancies 3) Run DOPE"
+            echo "2) Download/Update DOPE 4) Quit"
+            ;;
+        "Run DOPE")
+            echo "you chose choice $REPLY which is $opt"
+            sleep 3
+            cd
+            chmod +x ./DOPE/DOPE.cli
+            ./DOPE/DOPE.cli
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
