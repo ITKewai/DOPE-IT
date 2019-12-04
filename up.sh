ee1f657e012f4dd3b395d9158485870cdaf04415
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2019-12-04 16:17:51 +0100

    new up.sh

--- up.sh
+++ up.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+if [ -f /etc/centos-release ]; then
+sudo yum install wget
+bash <(wget -qO- https://raw.githubusercontent.com/ITKewai/DOPE-IT-PRIVATE/master/installer-dope.sh) 
+else
 sudo apt install wget
-bash <(wget -qO- https://raw.githubusercontent.com/ITKewai/DOPE-IT/master/installer-dope.sh) 
+bash <(wget -qO- https://raw.githubusercontent.com/ITKewai/DOPE-IT-PRIVATE/master/installer-dope.sh) 
+fi
+exit
