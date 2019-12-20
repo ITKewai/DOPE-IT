bd27a41584a43261b9f8dcd7eac6e0f27575bf54
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2019-12-20 20:05:39 +0100

    Update up.sh

--- up.sh
+++ up.sh
@@ -4,6 +4,7 @@ sudo yum install wget
 bash <(wget -qO- https://raw.githubusercontent.com/ITKewai/DOPE-IT-PRIVATE/master/installer-dope.sh) 
 else
 sudo apt install wget
+clear
 bash <(wget -qO- https://raw.githubusercontent.com/ITKewai/DOPE-IT-PRIVATE/master/installer-dope.sh) 
 fi
 exit
