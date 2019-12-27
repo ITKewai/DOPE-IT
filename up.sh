b46eb87480917d3eaa979cbae64d04cfbc6df6ed
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2019-12-27 19:40:05 +0100

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 if [ -f /etc/centos-release ]; then
-sudo yum install wget
 bash <(wget -qO- https://raw.githubusercontent.com/ITKewai/DOPE-IT/master/installer-dope.sh) 
 else
-sudo apt install wget
-clear
 bash <(wget -qO- https://raw.githubusercontent.com/ITKewai/DOPE-IT/master/installer-dope.sh) 
 fi
 exit
